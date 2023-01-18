2d1057b34fadd6c56730e30ac147ad1f7bb52505
Amanda Souza <amandasrs1001@gmail.com>
2023-01-18 14:51:34 -0300

    Defined Fetch for Getting Meals

--- src/App.js
+++ src/App.js
@@ -5,30 +5,9 @@ import Modal from "./Components/UI/Modal";
 import React, {useState} from "react";
 import CartProvider from "./Store/cart-provider";
 
-const Meals = [{
-  id: 'g1',
-  name: 'Sushi',
-  price: 12.45,
-  description: 'Fresh fish and veggies'
-}, {
-  id: 'g2',
-  name: 'Barbecue Burguer',
-  price: 30.21,
-  description: 'American, raw, meaty'
-}, {
-  id: 'g3',
-  name: 'Green Bowl',
-  price: 7.10,
-  description: 'Healthy... green...'
-}, {
-  id: 'g4',
-  name: 'Popcorn',
-  price: 5.50,
-  description: 'Perfect for the movies!'
-}]
 
 function App() {
-    
+
     const [ModalSets, SetModalSets] = useState(false)
     const ShowModalSet = () => {
       SetModalSets(true)
@@ -42,7 +21,7 @@ function App() {
       <Header SetModalShow={ShowModalSet}></Header>
       <main>
       <MainSummary></MainSummary>
-      <MealList AllMeals={Meals}></MealList>
+      <MealList></MealList>
       </main>
       {ModalSets && <Modal SetModalClose={CloseModalSet}></Modal>}
     </CartProvider>
--- src/Components/Meals/MealList.js
+++ src/Components/Meals/MealList.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem";
 import styles from './MealList.module.css'
 
 const MealList = (props) => {
+
+    const [Meals, SetMeals] = useState([])
+    const [IsLoading, SetIsLoading] = useState(false)
+
+    const LoadedMeals = []
+  
+    const MealsHandler = () => {
+        SetIsLoading(true)
+
+        fetch('https://react-meals-5fc2d-default-rtdb.firebaseio.com/meals.json')
+        .then(response => {return response.json()})
+        .then(data => { for (const id in data) {
+            LoadedMeals.push({
+            key: id,
+            name: data[id].name,
+            price: data[id].price,
+            description:data[id].description
+         })}
+        return LoadedMeals
+        })
+        .then(data => SetMeals(data))
+    } 
     
     const OnManageList = (data => 
         props.OnManageMeals(data))
 
     return <div className={styles.meals}>
         <Card>
+            <div className={styles.FetchState}>
+            {(Meals.length == 0 && !IsLoading)  && <button className={styles.FetchButton} onClick={MealsHandler}>Acess the Meals list</button>}
+            {(IsLoading && Meals.length == 0)  && <h1>We are loading your data!</h1>}
+            </div>
             <ul>
-                {props.AllMeals.map((meal) => { return <MealItem OnManageList={OnManageList} 
-                    key={meal.id}
-                    id={meal.id}
+                {Meals.map((meal) => { return <MealItem OnManageList={OnManageList} 
+                    key={meal.key}
+                    id={meal.key}
                     name={meal.name}
                     price={meal.price}
                     description={meal.description}></MealItem>
